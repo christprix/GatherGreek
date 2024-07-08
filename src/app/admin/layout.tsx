1fd61393bdb6a2f7f0ac300bdc85f8db8b0848b6
Christian St. Prix <christprix@gmail.com>
2024-07-08 14:00:36 -0400

    remove admin

--- src/app/admin/layout.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function AdminLayout({children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-    return <>
-        <div className="container my-6">{children}</div>
-    </>
-}
