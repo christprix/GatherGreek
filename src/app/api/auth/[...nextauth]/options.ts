be0a8d3c82720d81b06f7f2d8323ed169eb0c7e5
Christian St. Prix <christprix@gmail.com>
2024-07-12 18:49:00 -0400

    remove github provider

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -8,10 +8,10 @@ import { compare } from "bcrypt";
 // const prismaclient = prisma;
 export const options: NextAuthOptions = {
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string,
-    }),
+    // GithubProvider({
+    //   clientId: process.env.GITHUB_ID as string,
+    //   clientSecret: process.env.GITHUB_SECRET as string,
+    // }),
     CredentialsProvider({
       name: "Credentials",
       credentials: {
