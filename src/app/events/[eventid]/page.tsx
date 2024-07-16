d894c7f3d510b06c8ceacd2edde56291ea4794b7
Christian St. Prix <christprix@gmail.com>
2024-07-16 15:59:34 -0400

    add carousel and create event page mobile view

--- src/app/events/[eventid]/page.tsx
+++ src/app/events/[eventid]/page.tsx
@@ -1,6 +1,16 @@
 import greekstep from "/public/greekstep-p01.jpg";
+import kappastep from "/public/kappastep.jpg";
+import deltastep from "/public/deltastep.jpg";
+import qstomp from "/public/qstomp.jpg";
+import alphastep from "/public/alphastep.jpg";
+import sgrhostep from "/public/sgrhostep.jpg";
+import zetastep from "/public/zetastep.jpg";
+
 import Image from "next/image";
 import Avatar from "@/components/Avatar";
+
+import Link from "next/link";
+import Nav from "@/components/Nav";
 const event = {
   title: "Xi Chi Sigma Step Show",
   location: "1 Amb Dr NW, Atlanta, GA 30313",
@@ -19,6 +29,70 @@ export default function EventDetails({
 }) {
   return (
     <>
+      {/* <Image
+        className="rounded-md md:hidden"
+        src={event.imagePath}
+        alt="event image"
+        width={1000}
+        height={1000}
+      ></Image> */}
+      <div className="flex md:hidden justify-center">
+        <div className="carousel rounded w-2/3">
+          <div className="carousel-item w-full glass">
+            <img
+              src={greekstep.src}
+              className="justify-center"
+              alt="Tailwind CSS Carousel component"
+            />
+            {/* <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+              <button className="btn btn-circle">❮</button>
+              <button className="btn btn-circle">❯</button>
+            </div> */}
+          </div>
+          <div className="carousel-item w-full">
+            <img
+              src={alphastep.src}
+              className="w-full"
+              alt="Tailwind CSS Carousel component"
+            />
+          </div>
+          <div className="carousel-item w-full">
+            <img
+              src={kappastep.src}
+              className="w-full"
+              alt="Tailwind CSS Carousel component"
+            />
+          </div>
+          <div className="carousel-item w-full">
+            <img
+              src={deltastep.src}
+              className="w-full"
+              alt="Tailwind CSS Carousel component"
+            />
+          </div>
+          <div className="carousel-item w-full">
+            <img
+              src={qstomp.src}
+              className="w-full"
+              alt="Tailwind CSS Carousel component"
+            />
+          </div>
+          <div className="carousel-item w-full">
+            <img
+              src={sgrhostep.src}
+              className="w-full"
+              alt="Tailwind CSS Carousel component"
+            />
+          </div>
+          <div className="carousel-item w-full">
+            <img
+              src={zetastep.src}
+              className="w-full"
+              alt="Tailwind CSS Carousel component"
+            />
+          </div>
+        </div>
+      </div>
       <div className="flex container m-4">
         <div id="left" className="grid flex-col flex-grow">
           <div className="text-2xl">{event.title}</div>
@@ -30,7 +104,7 @@ export default function EventDetails({
           <div>{event.price}</div>
           <button className="btn btn-primary">Get Tickets</button>
         </div>
-        <div id="right" className="grid flex-grow">
+        <div id="right" className="grid hidden md:block flex-grow">
           <div className="p-4">
             <Image
               className="rounded-md"
