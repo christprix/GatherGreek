8eadcc0605b85d0a22a2b76b3170651f11ee9c5d
Christian St. Prix <christprix@gmail.com>
2024-07-17 16:15:31 -0400

    added key to fix map error

--- src/components/Taglist.tsx
+++ src/components/Taglist.tsx
@@ -33,7 +33,7 @@ const importedtags = [element, element2, element3, element4, element5];
 export default function Taglist() {
   const tags = importedtags.map((t: any, index: number) => {
     return (
-      <div className="flex-col">
+      <div className="flex-col" key={index}>
         <div className="avatar m-1 flex-col">
           <div className="p-4 hover:p-3 border-2 w-16 rounded-full ring-offset-2">
             <div key={index}>{t.icon}</div>
