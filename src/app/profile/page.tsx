0ca58a07e61f0da7a06c4f76bb38553126c1c5cf
Christian St. Prix <christprix@gmail.com>
2024-07-19 16:31:18 -0400

    add hero pic

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -14,6 +14,7 @@ import sigmavolunteer from "/public/sigmavolunteer.jpg";
 import sgrhoevent from "/public/sgrhoevent.jpg";
 import sigmabrotherhood from "/public/sigmabrotherhood.jpg";
 import qstomp from "/public/qstomp.jpg";
+import blackpeople2 from "/public/blackpeople2.jpg";
 import SideCardlist from "@/components/SideCardList";
 
 const anton = Anton({
@@ -26,13 +27,41 @@ export default async function Page() {
   const session = await getServerSession(options);
   return (
     <div className="bg-base-200 p-4">
-      <Image
+      {/* <Image
         src={Saint.src}
         width={700}
-        height={500}
+        height={600}
         alt="image"
-        className="rounded md:hidden block"
-      ></Image>
+        className=" md:hidden block "
+      ></Image> */}
+      <div
+        className="hero h-80 rounded-md"
+        style={{
+          backgroundImage: `url(${sigmabrotherhood.src})`,
+        }}
+      >
+        <div className="hero-overlay bg-opacity-50"></div>
+        <div className="mt-40 md:hidden text-neutral-content">
+          <div className="max-w-sm">
+            <h2 className="card-title text-xs">
+              Welcome Back, Christian St. Prix
+            </h2>
+            <div className="text-sm flex flex-row">
+              <FontAwesomeIcon icon={faLocationDot} className="w-3 mx-1" />
+              <div>Douglasville, GA</div>
+            </div>
+            <div className="text-sm flex flex-row">
+              <FontAwesomeIcon icon={faPeopleGroup} className="w-3 mx-1" />
+              <div>Phi Beta Sigma, Mu Epsilon Chapter</div>
+            </div>
+            <div className="text-sm flex flex-row">
+              <FontAwesomeIcon icon={faUserGraduate} className="w-3 mx-1" />
+              <div>Florida State University</div>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <div className="flex">
         <div className="hidden md:block card bg-base-100 w-96 h-fit m-3 ">
           <figure className="px-10 pt-10">
@@ -66,60 +95,6 @@ export default async function Page() {
       <div className="m-4 text-3xl">Past Events</div>
       <div className="m-5 flex overflow-x-auto justify-around">
         {/* turn into component */}
-        <div className="card bg-base-100 min-w-48 shadow-xl m-2">
-          <figure className="px-10 pt-10">
-            <img
-              src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-              alt="Shoes"
-              className="rounded-xl"
-            />
-          </figure>
-          <div className="card-body items-center text-center">
-            <h2 className="card-title">Xi Chi Sigma</h2>
-            <p className="text-xs">
-              If a dog chews shoes whose shoes does he choose?
-            </p>
-            <div className="card-actions">
-              <button className="btn btn-primary">View Events</button>
-            </div>
-          </div>
-        </div>
-        <div className="card bg-base-100 min-w-48 shadow-xl m-2">
-          <figure className="px-10 pt-10">
-            <img
-              src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-              alt="Shoes"
-              className="rounded-xl"
-            />
-          </figure>
-          <div className="card-body items-center text-center">
-            <h2 className="card-title">Xi Chi Sigma</h2>
-            <p className="text-xs">
-              If a dog chews shoes whose shoes does he choose?
-            </p>
-            <div className="card-actions">
-              <button className="btn btn-primary">View Events</button>
-            </div>
-          </div>
-        </div>
-        <div className="card bg-base-100 min-w-48 shadow-xl m-2">
-          <figure className="px-10 pt-10">
-            <img
-              src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-              alt="Shoes"
-              className="rounded-xl"
-            />
-          </figure>
-          <div className="card-body items-center text-center">
-            <h2 className="card-title">Xi Chi Sigma</h2>
-            <p className="text-xs">
-              If a dog chews shoes whose shoes does he choose?
-            </p>
-            <div className="card-actions">
-              <button className="btn btn-primary">View Events</button>
-            </div>
-          </div>
-        </div>
         <div className="card bg-base-100 min-w-48 shadow-xl m-2">
           <figure className="px-10 pt-10">
             <img
