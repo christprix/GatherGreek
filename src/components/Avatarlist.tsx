32d6b9b8eec8818cdc001f678d9e976fbc0fc4c0
Christian St. Prix <christprix@gmail.com>
2024-07-29 21:37:05 -0400

    create form actions with some client validations

--- src/components/Avatarlist.tsx
+++ src/components/Avatarlist.tsx
@@ -1,20 +1,22 @@
+import xichilogo from "/public/xichilogo.jpeg";
+
 export default function Avatarlist() {
   return (
     <>
       <div className="avatar-group -space-x-6 rtl:space-x-reverse">
         <div className="avatar">
           <div className="w-12">
-            <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+            <img src={xichilogo.src} />
           </div>
         </div>
         <div className="avatar">
           <div className="w-12">
-            <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+            <img src={xichilogo.src} />
           </div>
         </div>
         <div className="avatar">
           <div className="w-12">
-            <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+            <img src={xichilogo.src} />
           </div>
         </div>
         <div className="avatar placeholder">
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,5 @@
 export { default } from "next-auth/middleware";
 
-export const config = { matcher: ["/profile", "/create_event"] };
+export const config = {
+  matcher: ["/profile", "/create_event", "/create_event/form"],
+};
