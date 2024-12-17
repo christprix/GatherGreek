f8a63a183619fb6a81ce5f72379b3830ea45ce37
Christian St. Prix <christprix@gmail.com>
2024-12-17 15:56:08 -0500

    check

--- src/components/eventsForm/ImageUploader.tsx
+++ src/components/eventsForm/ImageUploader.tsx
@@ -8,16 +8,16 @@ type Uploadresults = {
   event: "success";
 };
 
-export default function ImageUploader({ eventId }: any) {
+export default function ImageUploader() {
   const [resource, setResource] = useState("");
   return (
     <>
       <CldUploadWidget
         uploadPreset="dbmlvgit"
-        onSuccess={(results: Uploadresults | any) => {
-          console.log("Public ID", results.info.public_id);
-          setResource(results.info.public_id);
-        }}
+        // onSuccess={(results: Uploadresults | any) => {
+        //   console.log("Public ID", results.info.public_id);
+        //   setResource(results.info.public_id);
+        // }}
       >
         {({ open }) => {
           return <button onClick={() => open()}>Upload an Image</button>;
