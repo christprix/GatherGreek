7ce47ab0406533ec63838b7b05d80ac1a8a26854
Christian St. Prix <christprix@gmail.com>
2025-02-05 19:52:25 -0500

    added it

--- src/app/api/auth/signin/page.tsx
+++ src/app/api/auth/signin/page.tsx
@@ -4,7 +4,7 @@ import { signIn } from "next-auth/react";
 import { useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 
-export default function SignIn(props) {
+export default function SignIn(props: any) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
