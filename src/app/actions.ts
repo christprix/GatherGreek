137213c297d4c8cfaa0536d702a0445b67c2ec1c
Christian St. Prix <46357071+christprix@users.noreply.github.com>
2025-12-01 17:32:22 -0500

    Update actions.ts
    Message change

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -53,7 +53,7 @@ export async function verifyTicket(qrCodeData: string, eventId: string) {
     }
 
     if (ticket.status === "used") {
-      return { success: false, message: "Ticket already used" };
+      return { success: false, message: "Ticket has been verified" };
     }
     return {
       success: true,
